5965a259e67295775c10e5f7756f6666f5a85eab
Alvin <ling94alvin@gmail.com>
2019-04-18 21:49:35 -0500

    giant weather models

--- Weather/Model/WeatherModel.swift
+++ Weather/Model/WeatherModel.swift
@@ -8,43 +8,83 @@
 
 import Foundation
 
-struct CurrentWeather {
+struct CurrentWeather: Codable {
+    let info: [Info]
+    let temp: Temp
+    let wind: Wind
+    let time: Int
+    let cityID: Int
+    let city: String
     
-    struct Info: Codable {
-        let weather: [Weather]
-        let main: Main
-        let wind: Wind
-        let dt: Int
-        let cityID: Int
-        let city: String
+    enum CodingKeys: String, CodingKey {
+        case info = "weather"
+        case temp = "main"
+        case wind
+        case time = "dt"
+        case cityID = "id"
+        case city = "name"
+    }
+}
+
+struct DailyWeather {
+    
+    struct Contents: Codable {
+        let count: Int
+        let forecasts: [Forecast]
+        let city: City
         
         enum CodingKeys: String, CodingKey {
-            case weather
-            case main, wind
-            case dt
-            case cityID = "id"
-            case city = "name"
+            case forecasts = "list"
+            case city
+            case count = "cnt"
         }
     }
     
-    struct Main: Codable {
-        let temp: Double
-        let tempMin, tempMax: Double
-        
-        enum CodingKeys: String, CodingKey {
-            case temp
-            case tempMin = "temp_min"
-            case tempMax = "temp_max"
-        }
-    }
-    
-    
-    struct Weather: Codable {
+    struct City: Codable {
         let id: Int
-        let main, description, icon: String
+        let name: String
+        let country: String
     }
+
+    struct Forecast: Codable {
+        let time: Int
+        let temp: Temp
+        let info: [Info]
+        let wind: Wind
+        let date: String
+        
+        enum CodingKeys: String, CodingKey {
+            case time = "dt"
+            case info = "weather"
+            case wind
+            case date = "dt_txt"
+            case temp = "main"
+        }
+    }
+}
+
+
+struct Info: Codable {
+    let state: String
+    let description: String
+    let icon: String
     
-    struct Wind: Codable {
-        let speed, deg: Double
+    enum CodingKeys: String, CodingKey {
+        case description, icon
+        case state = "main"
     }
+}
+
+struct Temp: Codable {
+    let curr, min, max: Double
+    
+    enum CodingKeys: String, CodingKey {
+        case curr = "temp"
+        case min = "temp_min"
+        case max = "temp_max"
+    }
+}
+
+struct Wind: Codable {
+    let speed, deg: Double
 }
