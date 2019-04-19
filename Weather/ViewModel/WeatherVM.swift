9627e3609d73efa2c8cdef26d81c50261ba557a8
Alvin <ling94alvin@gmail.com>
2019-04-19 15:08:35 -0500

    weather view model, still needs batch fetch functionality

--- Weather/ViewModel/WeatherVM.swift
+++ Weather/ViewModel/WeatherVM.swift
@@ -7,3 +7,40 @@
 //
 
 import Foundation
+
+class WeatherVM {
+    
+    private let service: WeatherService
+    private var data: [CurrentWeather] = []
+    
+    var numOfCities: Int {
+        return data.count
+    }
+    
+    init(_ service: WeatherService) {
+        self.service = service
+    }
+    
+    func fetchData(_ lat: Double, _ lon: Double, completion: @escaping (Error?) -> Void) {
+        service.fetchData(lat: lat, lon: lon, type: .current) { (results: CurrentWeather?, error) in
+            if let results = results {
+                self.data.append(results)
+                completion(nil)
+            } else { completion(error) }
+        }
+    }
+    
+    func fetchData(cityIDs: [Int], completion: @escaping (Error?) -> Void) {
+        
+    }
+    
+    func weather(at indexPath: IndexPath) -> CurrentWeather {
+        return weather(at: indexPath.row)
+    }
+    
+    func weather(at index: Int) -> CurrentWeather {
+        return data[index]
+    }
+    
+    
+}
