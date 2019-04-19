1969194e4e4fed19800ce89efa9b482d4445b1ca
Alvin <ling94alvin@gmail.com>
2019-04-19 11:12:07 -0500

    google place picker

--- Weather/CustomClasses/GMSPlacePicker.swift
+++ Weather/CustomClasses/GMSPlacePicker.swift
@@ -6,4 +6,32 @@
 //  Copyright © 2019 iOSPlayground. All rights reserved.
 //
 
-import Foundation
+import GooglePlaces
+
+class GMSPlacePicker: GMSAutocompleteViewController, GMSAutocompleteViewControllerDelegate {
+    
+    var selectPlaceAction: ((GMSPlace) -> Void)?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        delegate = self
+        placeFields = GMSPlaceField(rawValue: UInt(GMSPlaceField.name.rawValue) | UInt(GMSPlaceField.coordinate.rawValue))!
+        let filter = GMSAutocompleteFilter()
+        filter.type = .noFilter
+        autocompleteFilter = filter
+    }
+    
+    
+    func viewController(_ viewController: GMSAutocompleteViewController, didAutocompleteWith place: GMSPlace) {
+        selectPlaceAction?(place)
+        viewController.dismiss(animated: true, completion: nil)
+    }
+    
+    func viewController(_ viewController: GMSAutocompleteViewController, didFailAutocompleteWithError error: Error) {
+        print("Error: ", error.localizedDescription)
+    }
+    
+    func wasCancelled(_ viewController: GMSAutocompleteViewController) {
+        viewController.dismiss(animated: true, completion: nil)
+    }
+}
