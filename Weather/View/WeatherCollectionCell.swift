8f6f715b3860edad0a65845ed757d3639251c341
Alvin <ling94alvin@gmail.com>
2019-04-20 17:14:53 -0500

    custom cells for collection view

---
+++ Weather/View/WeatherCollectionCell.swift
@@ -0,0 +1,18 @@
+//
+//  WeatherCollectionCell.swift
+//  Weather
+//
+//  Created by Alvin Ling on 4/20/19.
+//  Copyright © 2019 iOSPlayground. All rights reserved.
+//
+
+import UIKit
+
+class WeatherCollectionCell: UICollectionViewCell {
+
+    @IBOutlet weak var info: WeatherInfoView!
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+
+}
