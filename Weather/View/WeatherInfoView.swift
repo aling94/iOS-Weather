d53f8611e15c8d1c0e7895eabdd45772d3dedf69
Alvin <ling94alvin@gmail.com>
2019-04-19 22:16:53 -0500

    set up views for second page display

--- Weather/View/WeatherInfoView.swift
+++ Weather/View/WeatherInfoView.swift
@@ -10,5 +10,22 @@ import UIKit
 
 class WeatherInfoView: UIView {
 
+    @IBOutlet weak var temp: UILabel!
+    @IBOutlet weak var state: UILabel!
+    @IBOutlet weak var cityName: UILabel!
+    @IBOutlet weak var wind: UILabel!
+    @IBOutlet weak var hiLow: UILabel!
+    @IBOutlet weak var icon: UIImageView!
     
+    func set(weatherData: CurrentWeather) {
+        temp?.text = "\(weatherData.temp.curr)"
+        cityName?.text = weatherData.city
+        wind?.text = "\(weatherData.wind.speed) m/s"
+        guard let info = weatherData.info.first else { return }
+        state?.text = info.state
+    }
+    
+    func set(weatherData: DailyWeather) {
+        
+    }
 }
