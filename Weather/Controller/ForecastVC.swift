2fb1f64496a71ce1fec3084829cd6d6e973230b7
Alvin <ling94alvin@gmail.com>
2019-04-20 12:47:58 -0500

    try out animations with card

--- Weather/Controller/ForecastVC.swift
+++ Weather/Controller/ForecastVC.swift
@@ -17,6 +17,11 @@ class ForecastVC: UIViewController {
     var forecastVM: ForecastVM!
     var forecastCard: ForecastCardVC!
     var handleHgt: CGFloat = 0
+    var cardHgt: CGFloat = 600
+    
+    var animations: [UIViewPropertyAnimator] = []
+    var animationProgress: CGFloat = 0
+    var cardExpanded = false
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -25,7 +30,7 @@ class ForecastVC: UIViewController {
         setupCard()
         loadForecasts()
         
-//        setupGestures()
+        setupGestures()
     }
     
     func loadForecasts() {
@@ -34,10 +39,7 @@ class ForecastVC: UIViewController {
                 self.showAlert(title: "Error", msg: error)
                 return
             }
-            
             DispatchQueue.main.async {
-                self.todayWeather.set(self.forecastVM.weather(at: 0))
-                self.tmrwWeather.set(self.forecastVM.weather(at: 1))
                 self.forecastCard.forecastVM = self.forecastVM
             }
         }
@@ -48,23 +50,72 @@ class ForecastVC: UIViewController {
         addChild(forecastCard)
         view.addSubview(forecastCard.view)
         handleHgt = forecastCard.handle.frame.height
-        forecastCard.view.frame = CGRect(x: 0, y: view.frame.height - handleHgt, width: view.frame.width, height: 600)
+        forecastCard.view.frame = CGRect(x: 0, y: view.frame.height - handleHgt, width: view.frame.width, height: cardHgt)
+        forecastCard.view.layer.cornerRadius = 8
         forecastCard.view.clipsToBounds = true
     }
     
     func setupGestures() {
         let pan = UIPanGestureRecognizer(target: self, action: #selector(handlePan))
-        let tap = UITapGestureRecognizer(target: self, action: #selector(handleTap))
+//        let tap = UITapGestureRecognizer(target: self, action: #selector(handleTap))
         
         forecastCard.handle.addGestureRecognizer(pan)
-        forecastCard.handle.addGestureRecognizer(tap)
+//        forecastCard.handle.addGestureRecognizer(tap)
+    }
+
+    
+    @objc func handlePan(sender: UIPanGestureRecognizer) {
+        switch sender.state {
+        case .began:
+            startTransition(duration: 0.9)
+        case .changed:
+            let translation = sender.translation(in: forecastCard.handle)
+            var fracCompleted = translation.y / cardHgt
+            fracCompleted = cardExpanded ? fracCompleted : -fracCompleted
+            updateTransiation(progress: fracCompleted)
+        case .ended:
+            continueTransition()
+        default: break
+        }
     }
     
-    @objc func handlePan(recognizer: UIPanGestureRecognizer) {
+    @objc func handleTap(sender: UITapGestureRecognizer) {
         
     }
     
-    @objc func handleTap(recognizer: UITapGestureRecognizer) {
-        
+    func animateTransition(duration: TimeInterval) {
+        guard animations.isEmpty else { return }
+        let hgtOffset = cardExpanded ? cardHgt : handleHgt
+        let frameAnimator = UIViewPropertyAnimator(duration: duration, dampingRatio: 1) {
+            self.forecastCard.view.frame.origin.y = self.view.frame.height - hgtOffset
+        }
+        frameAnimator.addCompletion { (position) in
+            self.cardExpanded = !self.cardExpanded
+            self.animations.removeAll()
+        }
+        frameAnimator.startAnimation()
+        animations.append(frameAnimator)
+    }
+    
+    func startTransition(duration: TimeInterval) {
+        if animations.isEmpty {
+            animateTransition(duration: duration)
+        }
+        for animator in animations {
+            animator.pauseAnimation()
+            animationProgress = animator.fractionComplete
+        }
+    }
+    
+    func updateTransiation(progress: CGFloat) {
+        for animator in animations {
+            animator.fractionComplete = progress + animationProgress
+        }
+    }
+    
+    func continueTransition() {
+        for animator in animations {
+            animator.continueAnimation(withTimingParameters: nil, durationFactor: 0)
+        }
     }
 }
