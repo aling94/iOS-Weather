9b5f593b07d8cdabf5d58227c8a6faf44438407e
Alvin <ling94alvin@gmail.com>
2019-04-19 21:25:55 -0500

    add current weather info view

---
+++ Weather/View/WeatherInfoView.swift
@@ -0,0 +1,14 @@
+//
+//  WeatherInfoView.swift
+//  Weather
+//
+//  Created by Alvin Ling on 4/19/19.
+//  Copyright © 2019 iOSPlayground. All rights reserved.
+//
+
+import UIKit
+
+class WeatherInfoView: UIView {
+
+    
+}
